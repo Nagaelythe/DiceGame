68963beaf3a5b1792200ac157366f5bb74a0dbc6
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2018-09-21 10:37:48 +0200

    Upd i pretty. lidt comentare

--- DiceGame.java
+++ DiceGame.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 
 public class DiceGame {
 
-    static int ONES =0; // number of ones rolled.
+    static int ONES = 0; // number of ones rolled.
     static ArrayList<Player> PLAYERS = new ArrayList<Player>(); // Players array.
     private static int tempBank = 0; // temp bank variable. Should be made local but not possible with current game loop.
     static boolean GAME = true; // gamestate
@@ -19,19 +19,17 @@ public class DiceGame {
 
     public static void main(String[] args) { // Main Game Loop
         //  UI.rules();
-       UI.menu(); // setting up the game.
-       if(!GAME){ // checking to se if we should run the game at all
-        return;
-       }
+        UI.menu(); // setting up the game.
+        if (!GAME) { // checking to se if we should run the game at all
+            return;
+        }
         UI.getPlayers(); // Initialising player array
         int players = PLAYERS.size();
-       
+
         while (GAME) {// main Game loop.
             turn(turnNumber % players, PLAYERS.get(turnNumber % players));
             gameIsDone(PLAYERS.get(turnNumber % players)); // Check if the game is done.
         }
-
-   
 
         if (WINNER.isBot) {
             DiceBot.Dance();
@@ -55,18 +53,11 @@ public class DiceGame {
             ROLLS.add(d6.roll());
         }
         UI.dispRolls();
-        
-        
-        if (ROLLS.contains(1) && sum(ROLLS) != RPT) {
-            System.out.println("You rolled a SINGLE 1, you lost " + tempBank + " points");
-            ROLLS.clear();
-            tempBank = 0;
-            ONES++;
-            turnNumber++;
-            p.updStreak();
 
-            if (!p.isBot) {
-                if (bot) {
+        if (ROLLS.contains(1) && sum(ROLLS) != RPT) {
+            lossTurn(p);
+            if (!p.isBot) { // checking if player is a bot.
+                if (bot) { // checking if there is a bot.
                     DiceBot.Taunt();
                 }
                 System.out.println("Pres enter to continue to next players turn:");
@@ -79,23 +70,32 @@ public class DiceGame {
             System.out.println("Giving you a total of " + (p.getBank() + tempBank) + " points.");
             System.out.println("Would you like to continue? ");
         }
-        
+
         Play(p);
-        
+
         ROLLS.clear();
         System.out.println("");
     }
-    
-    public static void endTurn(Player p){
-                p.addToBank(tempBank);
-                turnNumber++;
-                System.out.println(p.name + " stops his turn and is now at " + p.getBank() + " points.");
-                tempBank = 0;
-                p.updStreak();
-                p.newTurn();
-        
+
+    public static void lossTurn(Player p) {
+        System.out.println("You rolled a SINGLE 1, you lost " + tempBank + " points");
+        ROLLS.clear();
+        tempBank = 0;
+        ONES++;
+        turnNumber++;
+        p.updStreak();
     }
-    
+
+    public static void endTurn(Player p) {
+        p.addToBank(tempBank);
+        turnNumber++;
+        System.out.println(p.name + " stops his turn and is now at " + p.getBank() + " points.");
+        tempBank = 0;
+        p.updStreak();
+        p.newTurn();
+
+    }
+
     public static int sum(ArrayList<Integer> list) {
         int sum = 0;
         for (int i : list) {
@@ -108,8 +108,8 @@ public class DiceGame {
         GAME = !(p.getBank() >= Goalpost);
         WINNER = p;
     }
-    
-    public static void Play(Player p){
+
+    public static void Play(Player p) {
         if (!p.isBot) {  // pllayer  action
             if (UI.getYN()) {
                 p.nxtTurn();
@@ -125,6 +125,5 @@ public class DiceGame {
             }
         }
     }
-    
 
 }
